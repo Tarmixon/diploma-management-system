1412273c02a0d09393b5e35c610e600a0707404b
Tarmixon <taras.masyovsky@gmail.com>
2026-05-25 14:27:48 +0300

    Update api.ts

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,9 +1,8 @@
-// client/src/api.ts
 import axios from 'axios';
 
-// Створюємо базовий екземпляр axios, який завжди звертатиметься до нашого бекенду
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  // Вставляємо пряме посилання на ваш живий бекенд
+  baseURL: 'https://diploma-management-system.onrender.com/api',
 });
 
 export default api;
