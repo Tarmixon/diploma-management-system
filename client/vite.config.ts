2345d610c977617ac1d5d376a7e9b2925169f637
Tarmixon <taras.masyovsky@gmail.com>
2026-09-19 00:46:33 +0300

    Add build configuration to vite.config.ts
    Added build configuration with chunk size warning limit.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,9 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // --- ДОДАЙТЕ ЦЕЙ БЛОК ---
   optimizeDeps: {
     include: ['recharts', 'lodash']
   }
-  // -----------------------
+  build: {
+    chunkSizeWarningLimit: 1000
+  }
+
 })
